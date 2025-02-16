3b1158adc46f2a37b2935fcb6cff5ddc9692d577
julia-grimaldi <jfgrimaldi28@gmail.com>
2025-02-15 21:57:50 -0300

    fix knn

--- src/traffic/ML/knn.py
+++ src/traffic/ML/knn.py
@@ -25,32 +25,32 @@ def train_knn(file_path):
     X = data.drop('label', axis=1) 
     y = data['label']
 
-    # Balancear as classes com SMOTE, evitar overfitting com classe dominante
-    smote = SMOTE(random_state=42)
-    X_resampled, y_resampled = smote.fit_resample(X, y)
-
     # Identificar colunas numéricas e categóricas (não numéricas)
-    numeric_columns = X_resampled.select_dtypes(include=[np.number]).columns
-    categorical_columns = X_resampled.select_dtypes(exclude=[np.number]).columns
+    numeric_columns = X.select_dtypes(include=[np.number]).columns
+    categorical_columns = X.select_dtypes(exclude=[np.number]).columns
 
     # Preencher valores ausentes (colunas numéricas)
     imputer = SimpleImputer(strategy='mean')
-    X_resampled[numeric_columns] = imputer.fit_transform(X_resampled[numeric_columns])
+    X[numeric_columns] = imputer.fit_transform(X[numeric_columns])
 
     # Normalizar os dados com minmax (colunas numéricas)
     scaler = MinMaxScaler()
-    X_resampled[numeric_columns] = scaler.fit_transform(X_resampled[numeric_columns])
+    X[numeric_columns] = scaler.fit_transform(X[numeric_columns])
 
     # Aplicar One-Hot Encoding às colunas categóricas (melhor estratégio)
-    encoder = OneHotEncoder(sparse=False, drop='first')  # drop='first' para evitar redundancias
-    data_encoded = pd.DataFrame(encoder.fit_transform(X_resampled[categorical_columns]),
+    encoder = OneHotEncoder(sparse=False, drop='first', handle_unknown='ignore')  # drop='first' para evitar redundancias
+    data_encoded = pd.DataFrame(encoder.fit_transform(X[categorical_columns]),
                                 columns=encoder.get_feature_names_out(categorical_columns))
     
-    data_combined = pd.concat([X_resampled[numeric_columns], data_encoded], axis=1)
+    data_combined = pd.concat([X[numeric_columns], data_encoded], axis=1)
+
+    # Balancear as classes com SMOTE, evitar overfitting com classe dominante
+    smote = SMOTE(random_state=42)
+    X_resampled, y_resampled = smote.fit_resample(data_combined, y)
 
     # Seleção de Atributos
-    selector = SelectKBest(chi2, k=min(10, data_combined.shape[1]))
-    X_selected = selector.fit_transform(data_combined, y_resampled)
+    selector = SelectKBest(chi2, k=min(10, X_resampled.shape[1]))
+    X_selected = selector.fit_transform(X_resampled, y_resampled)
 
     X_train, X_test, y_train, y_test = train_test_split(X_selected, y_resampled, test_size=0.3, random_state=42)
 
