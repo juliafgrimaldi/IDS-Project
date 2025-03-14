2b10f1d031f1d77dba8c016f2c50c2546bcc2374
julia-grimaldi <jfgrimaldi28@gmail.com>
2025-03-13 22:12:20 -0300

    fix controller

--- src/traffic/controller.py
+++ src/traffic/controller.py
@@ -139,7 +139,7 @@ class TrafficMonitor(app_manager.RyuApp):
             for dp in self.datapaths.values():
                 self._request_stats(dp)
             hub.sleep(10)
-            self.random_forest_predict()
+            self.predict_traffic()
 
     def _request_stats(self, datapath):
         self.logger.info('Sending flow stats request to: %016x', datapath.id if datapath.id else 0)
